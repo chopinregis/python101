6c9000c5387a35f131adcab095df818bf8f63ff8
chopinregis <regisndetene@gmail.com>
2024-06-12 19:27:43 -0400

    first test push

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("World"))
+
+print(greet("Alice"))
+
+def how_much_money_do_i_have(money):
+    return f"I have {money} dollars."
+
+print(how_much_money_do_i_have(100))
+
+print(how_much_money_do_i_have(200))
